3d5248168c829e1c010ce90b4674850c6a8c334f
Daniele Rengel <daniele.pazinatto@unioeste.br>
2017-10-17 14:42:57 -0200

    Faculdade

--- src/br/danieleaula/Login.java
+++ src/br/danieleaula/Login.java
@@ -5,14 +5,33 @@ import javax.swing.JOptionPane;
 public class Login {
 
 	public static void main(String[] args) {
-		
-		String usuarioBd;
-		String senhaBd; 
-		
-		
-		String usuario = JOptionPane.showInputDialog ("Usuário ");
-		String senha = JOptionPane.showInputDialog	("Senha ");	
-		
+
+		String usuarioBd = "DaniRengel";
+		String senhaBd = "senha123";
+		boolean senhaCorreta = false;
+
+		int i = 0;
+
+		while (i < 3 && !senhaCorreta) {
+
+			String usuario = JOptionPane.showInputDialog("Usuário ");
+			String senha = JOptionPane.showInputDialog("Senha ");
+
+			if (!usuarioBd.equals(usuario) && !senhaBd.equals(senha)) {
+				i++;
+				JOptionPane.showMessageDialog(null, "Senha Incorreta. " + i);
+				
+			} else {
+				senhaCorreta = true;
+			}
+
+		}
+		if (senhaCorreta) {
+			JOptionPane.showMessageDialog(null, "Senha correta. Usuário logado ");
+		} else {
+			JOptionPane.showMessageDialog(null, "Senha Incorreta. Conta bloqueada  " + i);
+
+		}
 	}
 
 }
